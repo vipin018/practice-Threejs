57126f13cae729ec7e66f74e6794636fad0eb247
vipin018 <vipinpathak018@gmail.com>
2025-04-11 20:58:36 +0530

    updated

---
+++ lumeo/src/main.js
@@ -0,0 +1,39 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+
+const canvas = document.querySelector('#canvas');
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+renderer.render(scene, camera);
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+window.addEventListener('resize', () => {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+});
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+
+const clock = new THREE.Clock();
+function animate() {
+  const elapsedTime = clock.getElapsedTime();
+  requestAnimationFrame(animate);
+  controls.update();
+  renderer.render(scene, camera);
+
+  cube.rotation.x = elapsedTime;
+}
+animate();
